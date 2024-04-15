2f6d630c80047fb70c27bdda8f4565df090b2510
Dunw1ch <142730530+Dunw1ch@users.noreply.github.com>
2024-04-14 19:01:08 -0700

    IPintel.py v0.05
    working on the write function, doesnt work

--- IPintel.py
+++ IPintel.py
@@ -18,7 +18,7 @@ if x == "y":
     headers = {
     "accept": "application/json",
     #add API key below
-    "x-apikey": ""
+    "x-apikey": "dd6921978ef32e2e9fe2494330f3b2061ffd65c6527740911d10bb06b90d1a6b"
     }
 
     response = requests.get(url, headers=headers)
@@ -28,7 +28,8 @@ if x == "y":
     obj = json.loads(info)
     json_formatted_str = json.dumps(obj, indent=0)
 
-    print(json_formatted_str)
-
+    f = open('VirusTotalreport.txt', 'w')
+    f.write(json_formatted_str)
+    f.close()
 
 input('Press ENTER to exit')
