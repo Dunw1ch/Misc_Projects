41433121070050381a308cc63e8cfbead58cdc1a
Dunw1ch <142730530+Dunw1ch@users.noreply.github.com>
2024-04-21 20:32:11 -0700

    Pathfinder.py
    messing with the tkinter module was wasting time, was able to create the same functionality in half the lines of code. Will revisit to add a way to update the user as the pathping is generated, also the main goal will be to print out pathping to a map, which will be hard.

--- Pathfinder.py
+++ Pathfinder.py
@@ -7,40 +7,17 @@
 
 from ip2geotools.databases.noncommercial import DbIpCity
 from geopy.distance import distance
-import tkinter 
+import subprocess
 
-window = tkinter.Tk()
-# Function to get the user input and print it
-def get_input():
-    ip_address = ip_entry.get()
-    
+ipaddr = input("enter an IP you would like to see the path to: ", )
 
-# Label widget to prompt the user
-label = tkinter.Label(window, text="Enter IP:")
-label.pack()
+print("is this the IP you would like to see the path to?", ipaddr)
 
-# Entry widget to get the input
-ip_entry = tkinter.Entry(window)
-ip_entry.pack()
-
-# Button to submit the input
-submit_button = tkinter.Button(window, text="Submit", command=get_input)
-submit_button.pack()
-
-def submit_button(button_press):
-    ping = tkinter.Label(window, text="Is this the IP you would like to search?", 'ip_address')
-    print("running pathping of", 'ip_address')
-
-quit = tkinter.Button(window, text="Quit", command = window.destroy).pack() 
-# Run the main event loop
-window.mainloop()
-
-child = pexpect.spawn('pathping -c 5 1.1.1.1',ip_address)
-
-'''
 confirm = input("y/n")
-    if confirm == ('n'):
-        print("ok, try another")
-    elif confirm == ('y'):
-        print("ok running pathping on:", ip_add)
-'''
+
+if confirm == ('n'):
+    print("ok, try another")
+elif confirm == ('y'):
+    print("ok running pathping on:", ipaddr)
+    result = subprocess.run(['pathping', '-n', ipaddr], capture_output=True, text=True, check=True)
+    print(result.stdout)
