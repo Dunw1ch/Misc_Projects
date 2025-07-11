477b5a349b865be740f25cf9688cf3f990d2a67f
Dunw1ch <neidetcherc@gmail.com>
2025-07-11 01:03:46 -0700

    initial commit

---
+++ Python Cybersecurity/RansomLetter.py
@@ -0,0 +1,16 @@
+from shodan import Shodan
+from shodan.cli.helpers import get_api_key
+
+api = Shodan(get_api_key())
+
+#incredibly important to add a limit to not overdue credit limit of 10k results a month
+limit = 1
+counter = 0
+
+
+for banner in api.search_cursor('has_screenshot:true encrypted attention'):
+    print(banner['data'])
+
+    counter += 1
+    if counter >= limit:
+        break
